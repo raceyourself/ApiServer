3b58895770321d7ae96d7ffc70e2d133009e240f
Danny Hawkins <danny.hawkins@gmail.com>
2013-09-07 21:32:20 +0100

    production settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ require 'bundler/capistrano'
 default_run_options[:pty] = true  # Must be set for the password prompt
                                   # from git to work
 set :application, "glassfit"
+set :deploy_to, File.join("", "var", "www", "vhosts", application)
 set :repository,  "git@github.com:glassfit/GFAuthenticate.git"
 set :scm, "git"
 set :user, "deployer"  # The server's user for deploys
@@ -12,9 +13,9 @@ set :deploy_via, :remote_cache
 
 set :ssh_options, { forward_agent: true }
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", primary: true # This is where Rails migrations will run
+role :web, "glassfit.dannyhawkins.co.uk"                          # Your HTTP server, Apache/etc
+role :app, "glassfit.dannyhawkins.co.uk"                         # This may be the same as your `Web` server
+role :db,  "glassfit.dannyhawkins.co.uk", primary: true # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
