21072ed374e8f0f6e63ea57cdf59aa719afa9e81
Danny Hawkins <danny.hawkins@gmail.com>
2013-09-08 16:49:18 +0100

    fixed api_test

--- api_test.rb
+++ api_test.rb
@@ -1,9 +1,8 @@
 #!/usr/bin/env ruby
-
 require 'oauth2'
 
 puts
-puts "============================== Databank client application token tester =============================="
+puts "============================== Glassfit client application token tester =============================="
 puts
 puts
 
@@ -18,26 +17,23 @@ client_id = default_value_or_input("Application's client ID", ARGV[0])
 secret = default_value_or_input("Application's client secret", ARGV[1])
 url = default_value_or_input("Databank url", "http://glassfit.dev")
 redirect_uri = default_value_or_input("Redirect URI", 'urn:ietf:wg:oauth:2.0:oob')
-locale = default_value_or_input("Locale", "en")
 puts 
 puts "Setting up client with following data: "
 puts "Client ID:       #{client_id}"
 puts "Client Secret:   #{secret}"
-puts "Databank URL:    #{url}"
+puts "Glassfit URL:    #{url}"
 puts "Redirect URI:    #{redirect_uri}"
-puts "Locale:          #{locale}" if locale
-puts 
 puts 
 
 
 @client = OAuth2::Client.new(client_id, secret, site: url)
-auth_url = @client.auth_code.authorize_url(redirect_uri: redirect_uri, scope: 'write', locale: locale)
+auth_url = @client.auth_code.authorize_url(redirect_uri: redirect_uri)
 
 puts "Go here: #{auth_url}"
 puts "Then enter the authorization code:"
 code = $stdin.gets
 
-token = @client.auth_code.get_token(code.strip, redirect_uri: redirect_uri, scope: 'write', locale: locale)
+token = @client.auth_code.get_token(code.strip, redirect_uri: redirect_uri)
 
 puts "Got new token: #{ token.inspect }"
 
@@ -47,19 +43,3 @@ loop do
   puts "Got response: "
   puts token.get('api/1/me').parsed
 end
-
-# puts "Got client, issuing account"
-# account = token.get('/api/v1/account').parsed
-# puts account.inspect
-
-# puts 'Updating name'
-# account = token.put('/api/v1/account', body: { user: { firstname: 'Danny', lastname: 'Hawkins' } } )
-# puts account.inspect
-
-# puts 'Updating password, enter current password'
-# current_password = gets
-
-# #account = token.put('/api/v1/password', body: { user: { password: 'password', password_confirmation: 'password', current_password: current_password} })
-# #puts account.inspect
-
-# token = token.refresh!
