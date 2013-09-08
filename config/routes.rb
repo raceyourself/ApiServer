b6a811060b455c8dc09a268bf4eda878e55a06c2
Danny Hawkins <danny.hawkins@gmail.com>
2013-09-08 01:56:34 +0100

    added registrations controller to allow params

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 GfAuthenticate::Application.routes.draw do
   use_doorkeeper
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations' }
 
   #TODO: should use authenticate blocks
 
