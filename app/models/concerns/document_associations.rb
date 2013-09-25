b66604d2c2a5f668decdfee1d8743659802084a5
Danny Hawkins <danny.hawkins@gmail.com>
2013-09-25 08:28:58 +0100

    comments on document associations concerns

--- app/models/concerns/document_associations.rb
+++ app/models/concerns/document_associations.rb
@@ -5,17 +5,14 @@ module Concerns
     included do
       # has many associations
       [:devices, :friends, :orientations, :positions, :tracks, :transactions].each do |assoc|
-
         # association getter
         define_method assoc do
           klass = "#{assoc.to_s}".classify.constantize
           klass.where(user_id: id)
         end
-
       end
 
-
-    end
+    end #included
 
   end
 end 
