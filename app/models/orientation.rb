9d0e412ab7009ff1fe677f8d63d8f47465412843
Ben <ben.lister@glassfitgames.com>
2013-10-08 15:24:57 +0100

    BL: updated model with new orientation columns

--- app/models/orientation.rb
+++ app/models/orientation.rb
@@ -1,15 +1,18 @@
 class Orientation < UserDocument
   # fields
+  field :id,         type: Integer
   field :track_id,   type: Integer
   field :ts,         type: DateTime
   field :roll,       type: Float
   field :pitch,      type: Float
   field :yaw,        type: Float
      
-  field :mag,        type: Array # [x,y,z]
-  field :acc,        type: Array # [x,y,z]
-  field :gyro,       type: Array # [x,y,z]
+  field :mag,        type: Array # [x,y,z] device co-ords
+  field :acc,        type: Array # [x,y,z] device co-ords
+  field :gyro,       type: Array # [x,y,z] device co-ords
+  field :rot,        type: Array # [a,b,c,d] rotation vector
+  field :linacc,     type: Array # [x,y,z] real-world co-ords
   # indexes
-  index track_id: 1
+  index id: 1
   # validations
 end
