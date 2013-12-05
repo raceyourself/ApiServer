2bdcf75e38dfb57fbe69db7b6941ffbc806cd9c8
husberg <janne@husberg.fi>
2013-12-05 17:15:49 +0000

    Don't cache api/1/me

--- app/controllers/api/credentials_controller.rb
+++ app/controllers/api/credentials_controller.rb
@@ -2,8 +2,6 @@ module Api
   class CredentialsController < BaseController
     doorkeeper_for :all
     
-    caches :show, caches_for: 5.minutes
-
     def show
       expose user, include: { authentications: { only: [ :provider, :uid, :permissions ] } }
     end
