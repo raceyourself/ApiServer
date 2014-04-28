9cdef370134de2af6bcca855946e090ac7af44f0
Janne Husberg <janne@husberg.fi>
2014-04-28 11:49:16 +0100

    Missing merge method

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -6,6 +6,20 @@ class Friendship < ActiveRecord::Base
   belongs_to :identity, :foreign_key => [:identity_type, :identity_uid]
   belongs_to :friend, :foreign_key => [:friend_type, :friend_uid], :class_name => Identity
 
+  def merge
+    hash = self.attributes.except('created_at', 'deleted_at', 'updated_at')
+    key = hash.extract!(*self.class.primary_key)
+    begin
+      o = self.class.find(key.values)
+      # Update
+      o.updated_at = Time.now
+      o.update!(hash)
+    rescue ActiveRecord::RecordNotFound => e
+      # Insert
+      self.save!
+    end
+  end
+
   def serializable_hash(options = {})
     options = {
        include: :friend, 
