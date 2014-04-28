1b2f0d62c0c5456a341c8c12c2b492df62fbffaf
Janne Husberg <janne@husberg.fi>
2014-04-28 13:24:24 +0100

    Twitter TypeError

--- app/models/twitter_identity.rb
+++ app/models/twitter_identity.rb
@@ -6,7 +6,7 @@ class TwitterIdentity < Identity
   end
 
   def update_from_twitter(data)
-    self.uid = data.id
+    self.uid = data.id.to_s
     self.name = data.name
     self.screen_name = data.screen_name
     self.photo = data.profile_image_url
