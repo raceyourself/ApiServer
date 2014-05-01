facc854a8a0ee8b9b906b6d8b2a4deb508e1e1c1
Ben Lister <ben.lister@cantab.net>
2014-05-01 18:08:14 +0100

    Added segment.io gem and user/event logging

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,23 @@ class Event < ActiveRecord::Base
     self.save!
     self
   end
+
+  after_commit :send_analytics, :on => :create
+
+  def send_analytics
+    logger.info("Event.send_analytics called")
+
+    Analytics.identify(
+      user_id: self.user_id,
+      traits: {
+        version: self.version,
+        device_id: self.device_id,
+        session_id: self.session_id,
+        data: data
+      },
+      timestamp: self.ts
+    )
+
+  end
+
 end
