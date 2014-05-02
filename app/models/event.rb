061a7a8caf491cae1bfefdd9af56f35db3bef253
Ben Lister <ben.lister@cantab.net>
2014-05-02 16:27:38 +0100

    Working segment.io Track calls

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,11 +10,13 @@ class Event < ActiveRecord::Base
 
   def send_analytics
     logger.info("Event.send_analytics called")
-    details = JSON.parse(self.data)
+    details = self.data
+
+    logger.info("Details is " + details.to_s)
 
     Analytics.track(
       user_id: self.user_id,
-      event: if details.key?(event_type) details.event_type else "unknown event"
+      event: details.key?(:event_type) ? details[:event_type] : "unknown event",
       properties: {
         version: self.version,
         device_id: self.device_id,
