f03930a1c544c6f08b44aac2d6a9871461121cee
Janne Husberg <janne@husberg.fi>
2014-05-02 08:29:07 +0100

    Apple push notifications bugfix

--- app/workers/push_notification_worker.rb
+++ app/workers/push_notification_worker.rb
@@ -14,7 +14,7 @@ class PushNotificationWorker
       logger.info response
     end
     # iOS devices
-    reg_ids = Device.where(user_id: user_id).where.not(:push_id => nil).where.not(:manufacturer => 'Apple').flat_map {|d| d.push_id}
+    reg_ids = Device.where(user_id: user_id).where.not(:push_id => nil).where(:manufacturer => 'Apple').flat_map {|d| d.push_id}
     unless reg_ids.empty?
       APNS.host = CONFIG[:apple][:apns_host]
       APNS.pem = CONFIG[:apple][:apns_pem]
