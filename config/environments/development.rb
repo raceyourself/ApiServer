7143341d1759bc306b18148ab5f7fc03a21b8861
Janne Husberg <janne@husberg.fi>
2014-05-16 14:52:32 +0100

    BUGFIX: Set asset_host for image uploads in all environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,6 @@ GfAuthenticate::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_controller.asset_host = "http://localhost:3000"
 end
