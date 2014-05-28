e6490dd6973fc7ca1767a58bbac1071b1d71f4fa
Janne Husberg <janne@husberg.fi>
2014-05-28 16:38:07 +0100

    BUGFIX: country_as_entered Merge in request fields

--- app/controllers/surveys/beta_insight_controller.rb
+++ app/controllers/surveys/beta_insight_controller.rb
@@ -13,18 +13,25 @@ module Surveys
                                 :gender, 
                                 :age_group, 
                                 :country, 
+                                :country_as_entered,
                                 :mobile_device_1, 
                                 :mobile_device_2, 
                                 :running_fitness, 
                                 :cycling_fitness, 
                                 :workout_fitness)
  
+      merged = {}
       # nested strong parameters with integer keys are broken for some reason
-      nested = {}
-      nested[:wearable_devices] = params.fetch(:wearable_devices) if params.has_key?(:wearable_devices)
-      nested[:goals] = params.fetch(:goals) if params.has_key?(:goals)
+      merged[:wearable_devices] = params.fetch(:wearable_devices) if params.has_key?(:wearable_devices)
+      merged[:goals] = params.fetch(:goals) if params.has_key?(:goals)
+      # request fields 
+      # TODO: allow these to be overriden if we upgrade to webhooks
+      merged[:time_submitted] = Time.now # TODO: format?
+      merged[:language] = request.env['HTTP_ACCEPT_LANGUAGE']
+      merged[:ip_address] = request.remote_ip
+      merged[:user_agent] = request.user_agent
       # save to db
-      i = SurveyBetaInsight.new(permitted.merge(nested))
+      i = SurveyBetaInsight.new(permitted.merge(merged))
       i.save!
 
       redirect_to "http://www.raceyourself.com/signed_up"
