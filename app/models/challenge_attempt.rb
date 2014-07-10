2029a33ef8b251a6560521d1722b306aab36f322
Janne Husberg <janne@husberg.fi>
2014-07-10 15:38:14 +0100

    BUGFIX: ChallengeAttempt foreign keys

--- app/models/challenge_attempt.rb
+++ app/models/challenge_attempt.rb
@@ -1,4 +1,4 @@
 class ChallengeAttempt < ActiveRecord::Base
-  belongs_to :challenge, :foreign_key => [:track_device_id, :challenge_id]
-  belongs_to :track, :foreign_key => [:challenge_device_id, :track_id]
+  belongs_to :challenge, :foreign_key => [:challenge_device_id, :challenge_id]
+  belongs_to :track, :foreign_key => [:track_device_id, :track_id]
 end
