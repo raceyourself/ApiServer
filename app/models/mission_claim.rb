de98394c4ba75af2683b0bb85b12e1d8cc544b63
Janne Husberg <janne@husberg.fi>
2014-07-18 17:12:14 +0100

    The usual deleted_at bugfix

--- app/models/mission_claim.rb
+++ app/models/mission_claim.rb
@@ -1,4 +1,6 @@
 class MissionClaim < ActiveRecord::Base
+  acts_as_paranoid
+
   belongs_to :mission_level, :foreign_key => [:mission_id, :level]
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717154000) do
+ActiveRecord::Schema.define(version: 20140718160621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -220,6 +220,7 @@ ActiveRecord::Schema.define(version: 20140717154000) do
     t.integer  "user_id",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
   end
 
   create_table "mission_levels", id: false, force: true do |t|
